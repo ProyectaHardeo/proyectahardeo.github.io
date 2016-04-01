2aa9c50c236fa278dde0e11e370cf76717079e24
ProyectaHardeo <proyecta.hardeo@gmail.com>
2016-04-01 18:26:35 -0430

    Update sendbymail.php

--- sendbymail.php
+++ sendbymail.php
@@ -1,9 +1,36 @@
 <?php
-echo 'Enviando correo';
-$to = "proyecta.hardeo@gmail.com";
-$subject = "Correo en prueba";
-$txt = "Hola es un placer saludar desde el correo en pruebas!";
-$headers = "From: aquiotrocorreo@ejemplo.com" . "\r\n" .
-"CC: yaquiotrocorreocomocc@aprenderaprogramar.com";
-mail($to,$subject,$txt,$headers);
+if(isset($_POST['email'])) {
+
+// Debes editar las próximas dos líneas de código de acuerdo con tus preferencias
+$to = "damian.hardeo@gmail.com";
+$subject = "Contacto desde el sitio web";
+
+// Aquí se deberían validar los datos ingresados por el usuario
+if(!isset($_POST['first_name']) ||
+!isset($_POST['last_name']) ||
+!isset($_POST['email']) ||
+!isset($_POST['telephone']) ||
+!isset($_POST['comments'])) {
+
+echo "<b>Ocurrió un error y el formulario no ha sido enviado. </b><br />";
+echo "Por favor, vuelva atrás y verifique la información ingresada<br />";
+die();
+}
+
+$email_message = "Detalles del formulario de contacto:\n\n";
+$email_message .= "Nombre: " . $_POST['first_name'] . "\n";
+$email_message .= "Apellido: " . $_POST['last_name'] . "\n";
+$email_message .= "E-mail: " . $_POST['email'] . "\n";
+$email_message .= "Teléfono: " . $_POST['telephone'] . "\n";
+$email_message .= "Comentarios: " . $_POST['comments'] . "\n\n";
+
+
+// Ahora se envía el e-mail usando la función mail() de PHP
+$headers = 'From: '.$email_from."\r\n".
+'Reply-To: '.$email_from."\r\n" .
+'X-Mailer: PHP/' . phpversion();
+mail($to, $subject, $email_message, $headers);
+
+echo "¡El formulario se ha enviado con éxito!";
+}
 ?>
